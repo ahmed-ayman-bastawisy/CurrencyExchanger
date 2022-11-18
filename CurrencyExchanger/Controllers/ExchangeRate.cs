943f3b744b3b62542cad04c5c2c80235e304cd1f
ahmed ayman <ahmed.ayman.bastawisy@outlook.com>
2022-11-19 00:32:35 +0300

    Add project files.

---
+++ CurrencyExchanger/Controllers/ExchangeRate.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyExchanger.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExchangeRate : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("convert")]
+        public ActionResult Convert(decimal amount, string from, string to, DateOnly? date = null)
+        {
+
+        }
+    }
+}
