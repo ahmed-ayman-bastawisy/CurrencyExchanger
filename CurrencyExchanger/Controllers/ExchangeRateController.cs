aa9fc213c7783eb62dab88fd20bfcc6730e9485c
ahmed ayman <ahmed.ayman.bastawisy@outlook.com>
2022-11-20 13:55:24 +0300

    - add real time rates api integration - add convert api integration - add logger - add postman api test collection and environment

---
+++ CurrencyExchanger/Controllers/ExchangeRateController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Web;
+
+namespace CurrencyExchanger.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class ExchangeRateController : ControllerBase
+    {
+        private readonly HttpClient _client;
+
+        public ExchangeRateController(IHttpClientFactory clientFactory)
+        {
+            _client = clientFactory.CreateClient("ExchangeRatesAPI");
+        }
+        /// <summary>
+        /// integrate with ExchangRates API, that returns real-time exchange rate data updated every 60 minutes, every 10 minutes or every 60 seconds.
+        /// </summary>
+        /// <param name="baseCurrency">Enter the three-letter currency code of your preferred base currency.</param>
+        /// <param name="symbols">Enter a list of comma-separated currency codes to limit output currencies.</param>
+        /// <returns>
+        /// success boolean, 
+        /// time stamp,
+        /// base currency,
+        /// date of the rates,
+        /// rates dictionary with currency symbol as a key and the rate as a value
+        /// </returns>
+        [HttpGet("rates")]
+        public async Task<ActionResult> GetRealTimeRates([FromQuery]string? baseCurrency = null, [FromQuery] string? symbols = null)
+        {
+            var uriBuilder = new UriBuilder(_client.BaseAddress + "/latest");
+            var requestQueryStrings = HttpUtility.ParseQueryString(string.Empty);
+            requestQueryStrings["symbols"] = symbols;
+            requestQueryStrings["base"] = baseCurrency;
+            uriBuilder.Query = requestQueryStrings.ToString();
+            var response = await _client.GetAsync(uriBuilder.ToString());
+            var responseConent = await response.Content.ReadAsStringAsync();
+            return StatusCode((int)response.StatusCode, responseConent);
+        }
+
+        /// <summary>
+        /// Currency conversion endpoint, which integrate with ExchangeRates API,
+        /// that can be used to convert any amount from one currency to another. 
+        /// In order to convert currencies, 
+        /// append the from and to parameters and set them to your preferred base and target currency codes.
+        /// </summary>
+        /// <param name="amount">The amount to be converted.</param>
+        /// <param name="from">The three-letter currency code of the currency you would like to convert from.</param>
+        /// <param name="to">The three-letter currency code of the currency you would like to convert to.</param>
+        /// <param name="date">Specify a date (format YYYY-MM-DD) to use historical rates for this conversion.</param>
+        /// <returns>
+        /// date if not specified then today date, 
+        /// info which holds the rate and time stamp.
+        /// query which holds the query string parameters amount, from, and to,
+        /// the result of the conversion,
+        /// and success boolean
+        /// </returns>
+        [HttpGet("exchange")]
+        public async Task<ActionResult> Convert([FromQuery] string amount, [FromQuery] string from, [FromQuery] string to, [FromQuery] string? date = null)
+        {
+            //bool requestValid = decimal.TryParse(amount , out decimal decAmount) && !string.IsNullOrEmpty(from) && !string.IsNullOrEmpty(to);
+            
+            var uriBuilder = new UriBuilder(_client.BaseAddress + "/convert");
+            var requestQueryStrings = HttpUtility.ParseQueryString(string.Empty);
+            requestQueryStrings["amount"] = amount;
+            requestQueryStrings["from"] = from;
+            requestQueryStrings["to"] = to;
+            requestQueryStrings["date"] = date;
+            uriBuilder.Query = requestQueryStrings.ToString();
+            var response = await _client.GetAsync(uriBuilder.ToString());
+            var responseConent = await response.Content.ReadAsStringAsync();
+            return StatusCode((int)response.StatusCode, responseConent);
+        }
+    }
+}
--- CurrencyExchanger/Program.cs
+++ CurrencyExchanger/Program.cs
@@ -1,4 +1,20 @@
+using Microsoft.Net.Http.Headers;
+using Serilog;
+
+
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+//create the logger and setup your sinks, filters and properties
+builder.Host.UseSerilog((hostContext, services, configuration) =>
+{
+    configuration.MinimumLevel.Debug()
+    .WriteTo.Console().WriteTo.File(@$"Logs/ExchangeRateLog-{DateTime.Now.Date.ToString("dd-MM-yyyy")}.txt"
+    , outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] [{SourceContext}] [{EventId}] {Message}{NewLine}{Exception}"
+    , rollingInterval: RollingInterval.Day,
+    rollOnFileSizeLimit: true, fileSizeLimitBytes: 1000000);
+});
 
 // Add services to the container.
 
@@ -6,6 +22,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddLogging();
+builder.Services.AddHttpClient("ExchangeRatesAPI", client => {
+    //add Rate Provider URI
+    client.BaseAddress = new Uri("https://api.apilayer.com/exchangerates_data");
+    //add Rate Provider API key to the default header
+    client.DefaultRequestHeaders.Add("apikey", "gTvO8ffdlaBNtTOzl2r6NECHPbgFExGL");
+    });
 
 var app = builder.Build();
 
